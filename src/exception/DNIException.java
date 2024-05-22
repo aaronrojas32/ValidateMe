ee753ea8eb333327bb2766f4137ea321576e6bb6
aaronrojas32 <56888910+aaronrojas32@users.noreply.github.com>
2024-05-22 12:31:51 +0200

    Create DNIException.java

---
+++ src/exception/DNIException.java
@@ -0,0 +1,54 @@
+package exception;
+
+/**
+ * The HourException class represents an exception related to hour operations.
+ * This exception may be thrown in situations where there are errors or issues
+ * with hour manipulation, parsing, or validation.
+ * 
+ * @version 1.0
+ * @since 2024-05-20
+ * @author Aarón Rojas
+ */
+public class DNIException extends Exception {
+
+	/**
+	 * Unique identifier for serialization.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor for HourException. Creates a new instance of
+	 * HourException with no detailed message.
+	 */
+	public DNIException() {
+		super();
+	}
+
+	/**
+	 * Constructor for HourException with a detailed message.
+	 * 
+	 * @param message The detailed message describing the cause of the exception.
+	 */
+	public DNIException(String message) {
+		super(message);
+	}
+
+	/**
+	 * Constructor for HourException with a root cause.
+	 * 
+	 * @param cause The root cause of the exception.
+	 */
+	public DNIException(Throwable cause) {
+		super(cause);
+	}
+
+	/**
+	 * Constructor for HourException with a detailed message and a root cause.
+	 * 
+	 * @param message The detailed message describing the cause of the exception.
+	 * @param cause   The root cause of the exception.
+	 */
+	public DNIException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
